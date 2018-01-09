f172f70290013a560f56f7f1813e32335b1e094a
Berkay Karataş <b2rky48@gmail.com>
2018-01-10 00:10:14 +0300

    Update Homepage.php

--- apps/karmate/controllers/Homepage.php
+++ apps/karmate/controllers/Homepage.php
@@ -13,23 +13,4 @@ class Homepage
 		];
 		>> new FW: APP\View('homepage', $homepageTransfers); <<
 	}
-	public function deneme($deneme)
-	{
-
-		>> $smart = new FW: SmartHttp\SmartHttp; <<
-		$smart->do = "get";
-		$smart->get_data = $deneme;
-		# If you do not use it will default to LINK_DATA.
-		$smart->SET = "INBOUND";
-		# If you do not use it, the default will be ",".
-		$smart->delimiter = '|';
-		#If you do not use it will default to ":".
-		$smart->equalizer = '=';
-		#IF WANTED TO ALLOW SOME HTML TAGS
-		$smart->allowed_html_tags = '<b></b><i></i>';
-		#
-		$smart->run();
-		echo @INBOUND["sa"];
-
-	}
 }
