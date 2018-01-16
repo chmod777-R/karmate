ee767bd728eb0b8019d80d3422738a2094c59ba8
insayd10 <b2rky48@gmail.com>
2018-01-16 14:35:59 +0300

    Comment Area added.

--- apps/karmate/config/controllerprogrammingshortcuts.php
+++ apps/karmate/config/controllerprogrammingshortcuts.php
@@ -1,3 +1,21 @@
 <?php return [
-    'sayHi'           =>      'echo "HI"'              
+    /**
+    * ----------------------------------------------------------------------
+    * @author Author
+    * @link   contact link
+    * @see    >> shortcut <<
+    * @since  File available since Karmate v 1.0.0
+    * ----------------------------------------------------------------------
+    * Controller programming shortcuts
+    * In a completely understandable way
+    * To do programming with the my own language
+    * I used these shortcuts.
+    * ----------------------------------------------------------------------
+    * This file allows me to use
+    * matches basic PHP usage.
+    * As a principle and a way of working
+    * There is no difference between basic PHP usage.
+    * ----------------------------------------------------------------------
+    */
+    'say'           =>      'echo'              
 ];
